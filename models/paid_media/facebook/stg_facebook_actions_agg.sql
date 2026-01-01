0a9086d628a42d7798b7d5ef14f5f845e452b880
Riley Sorenson <riley@resorensonllc.com>
2026-01-01 18:21:57 +0000

    fix fb conversions

--- models/paid_media/facebook/stg_facebook_actions_agg.sql
+++ models/paid_media/facebook/stg_facebook_actions_agg.sql
@@ -1,7 +1,23 @@
+WITH all_actions AS (
+    SELECT DATE
+         , AD_ID
+         , ACTION_TYPE
+         , VALUE
+    FROM `facebook_ads.ads_insights_actions`
+    
+    UNION ALL
+    
+    SELECT DATE
+         , AD_ID
+         , ACTION_TYPE
+         , VALUE
+    FROM `facebook_ads.ads_insights_conversions`
+)
+
 select ad_id,
         date,
         action_type,
         sum(value) as total_value,
         count(*) over (partition by ad_id, date) as action_count
-    from `facebook_ads.ads_insights_actions`
+    from all_actions
     group by ad_id, date, action_type
